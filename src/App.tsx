3474590e0d0cc34e13d0d0ca5e34a3c71115c519
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-09-23 22:19:22 +0530

    Implement responsive layout with DashboardMenu sidebar and fix layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
+import DashBoardMenu from "./components/DashBoardMenu";
 
 import Home from "./pages/Home.tsx";
-import './App.css'
+import './App.css';
 
 function App() {
+    return (
+        <div className="flex flex-col min-h-screen">
+            {/* Navbar - Full width */}
+            <Navbar />
 
+            {/* Main content area: Sidebar + Home Page */}
+            <div className="flex flex-1">
+                {/* Sidebar Menu */}
+                <DashBoardMenu />
 
-  return (
-      <div>
-          <Navbar/>
-          <Home/>
-          <Footer/>
-      </div>
-  )
+                {/* Main Content */}
+                <div className="flex-1 p-4 bg-gray-100">
+                    <Home />
+                </div>
+            </div>
+
+            {/* Footer - Full width */}
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
